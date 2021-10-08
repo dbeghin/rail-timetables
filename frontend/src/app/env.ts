476c414787e68949a6b136da764cbe3af4a75fd6
dbeghin <diego.beghin@gmail.com>
2021-10-08 12:07:57 +0200

    testing API address

--- frontend/src/app/env.ts
+++ frontend/src/app/env.ts
@@ -1,2 +1,3 @@
 //export const API_URL = 'http://localhost:8888';
-export const API_URL = 'http://127.0.0.1:8888';
+//export const API_URL = 'http://127.0.0.1:8888';
+export const API_URL = 'http://ec2-35-157-68-215.eu-central-1.compute.amazonaws.com:8888';
